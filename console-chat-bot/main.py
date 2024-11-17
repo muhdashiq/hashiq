5732a0c4d75afcb826b5de39f7216f576019b06e
Muhammed Ashiq <muhdashiq@gmail.com>
2024-11-17 20:40:57 +0530

    Updating with verbose and Summary memory

--- console-chat-bot/main.py
+++ console-chat-bot/main.py
@@ -1,17 +1,25 @@
 from langchain_community.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 from langchain.prompts import MessagesPlaceholder, HumanMessagePromptTemplate, ChatPromptTemplate
-from langchain.memory import ConversationBufferMemory, FileChatMessageHistory
+# from langchain.memory import ConversationBufferMemory, FileChatMessageHistory
+from langchain.memory import ConversationSummaryMemory, FileChatMessageHistory
 from dotenv import load_dotenv
 
 # Loading the environment variable for the API Keys
 load_dotenv()
 
-chat = ChatOpenAI()
-memory = ConversationBufferMemory(
-    chat_memory=FileChatMessageHistory("message-history.json"),
+chat = ChatOpenAI(verbose=True)
+# memory = ConversationBufferMemory(
+#     chat_memory=FileChatMessageHistory("message-history.json"),
+#     memory_key="messages",
+#     return_messages=True
+# )
+
+memory = ConversationSummaryMemory(
     memory_key="messages",
-    return_messages=True
+    return_messages=True,
+    llm=chat,
+    verbose=True
 )
 
 prompt = ChatPromptTemplate(
